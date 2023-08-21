0b721fa76f08adf694abf3579783be9bfe838586
xionglee01 <74166763+xionglee01@users.noreply.github.com>
2023-08-21 15:36:11 -0700

    Added eraser button
    allow user to access an eraser to erase pixels. Added a footer at the bottom. Set active class to button to show user they are
    using eraser or pen.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,10 @@ const container = document.querySelector(".container");
 const slider = document.getElementById("gridRange");
 const output = document.querySelector(".out");
 const colorpicker = document.getElementById("colorpicker");
+const pen = document.getElementById("draw");
+const eraser = document.getElementById("erase"); 
 
+let erase = false;
 
 let row = slider.value;
 let col = slider.value;
@@ -52,22 +55,61 @@ function changeGrid()
 
 function changeColor(e)
 {
-    if(e.target.className === 'box')
-    {
-        e.target.style.backgroundColor = colorpicker.value;
-    }
-}
-
-function changeColorHold(e)
-{
-    if(e.buttons > 0)
+    if(erase === false)
     {
         if(e.target.className === 'box')
         {
             e.target.style.backgroundColor = colorpicker.value;
         }
     }
+    else
+    {
+        if(e.target.className === 'box')
+            e.target.style.backgroundColor = "white";
+    }
 }
+
+function changeColorHold(e)
+{
+    if(erase === false)
+    {
+        if(e.buttons > 0)
+        {
+            if(e.target.className === 'box')
+            {
+                e.target.style.backgroundColor = colorpicker.value;
+            }
+        }
+    }
+    else
+    {
+        if(e.buttons > 0)
+        {
+            if(e.target.className === 'box')
+            {
+                e.target.style.backgroundColor = "white";
+            }
+        }
+    }
+}
+
+pen.addEventListener( "click", () => {
+    if(erase === true)
+    {
+        erase = false;
+    }
+    pen.classList.add("active")
+    eraser.classList.remove("active");
+});
+
+eraser.addEventListener("click", () =>
+{
+    if(erase === false)
+        erase = true;
+    pen.classList.remove("active")
+    eraser.classList.add("active");
+});
+
 
 slider.oninput = changeGrid;
 grid.forEach(box => addEventListener('mousedown', changeColor));
